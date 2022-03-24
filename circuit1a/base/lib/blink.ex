a5af1eed8fc610888d2bb40f276e8dfac90bc103
Samuel Mullin <github@sam.justsam.ca>
2022-03-23 21:47:28 -0300

    Fix syntax errors in blink and supervisor

--- circuit1a/base/lib/blink.ex
+++ circuit1a/base/lib/blink.ex
@@ -19,18 +19,18 @@ defmodule Circuit1a.Blink do
   @impl true
   def init(_) do
     {:ok, output_gpio} = GPIO.open(output_gpio(), :output)
-    Process.send_after(self(), {:led_on}, 100)
+    Process.send_after(self(), :led_on, 100)
     {:ok, %{output_gpio: output_gpio}}
   end
 
   @impl true
-  def handle_info({:led_on}, %{output_gpio: output_gpio}  = state) do
+  def handle_info(:led_on, %{output_gpio: output_gpio}  = state) do
     led_on(output_gpio, @blink_ms)
     {:noreply, state}
   end
 
   @impl true
-  def handle_info({:led_off}, %{output_gpio: output_gpio}  = state) do
+  def handle_info(:led_off, %{output_gpio: output_gpio}  = state) do
     led_off(output_gpio, @blink_ms)
     {:noreply, state}
   end
@@ -38,12 +38,12 @@ defmodule Circuit1a.Blink do
   # --- Private Implementation ---
   defp led_on(output_gpio, blink_ms) do
     GPIO.write(output_gpio, 1)
-    Process.send_after(self(), {:led_off}, blink_ms)
+    Process.send_after(self(), :led_off, blink_ms)
   end
 
   defp led_off(output_gpio, blink_ms) do
     GPIO.write(output_gpio, 0)
-    Process.send_after(self(), {:led_on}, blink_ms)
+    Process.send_after(self(), :led_on, blink_ms)
   end
 
   defp output_gpio, do: Application.get_env(:circuit1a, :blink_output_gpio)
